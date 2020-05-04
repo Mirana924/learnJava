7904027859d652e239733f386ce2a13e436889c8
Yiru (Mirana) Yang <47059997+Mirana924@users.noreply.github.com>
2020-05-04 16:27:32 -0700

    main: multiple questions challenge

--- src/Main.java
+++ src/Main.java
@@ -1,17 +1,25 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
+        String question = "Which country is the third-largest country?";
+        String choiceOne = "USA";
+        String choiceTwo = "China";
+        String choiceThree = "Australia";
+
+        String correctAnswer = choiceTwo;
+
+        System.out.println(question);
+        System.out.println(choiceOne);
+        System.out.println(choiceTwo);
+        System.out.println(choiceThree);
+
         Scanner input = new Scanner(System.in);
-        boolean isOnRepeat = true;
+        String userInput = input.next();
 
-        while(isOnRepeat) {
-            System.out.println("would you like to take this song off of repeat? if so, answer yes");
-            String userInput = input.next();
-
-            if (userInput.equals("yes")) {
-                isOnRepeat = false;
-            }
+        if (userInput.equals("China")) {
+            System.out.println("Congrats! Your answer is correct!");
+        } else {
+            System.out.println("Sorry, the answer is not right.");
         }
-        System.out.println("play next song");
     }
 }
