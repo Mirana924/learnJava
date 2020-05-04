56f48e9e939b97a643a8ddf6a9b3bcb57f4f3e37
Yiru (Mirana) Yang <47059997+Mirana924@users.noreply.github.com>
2020-05-04 16:14:44 -0700

    main: while loop

--- src/Main.java
+++ src/Main.java
@@ -1,14 +1,17 @@
 import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Pick a number between 1 and 10");
         Scanner input = new Scanner(System.in);
+        boolean isOnRepeat = true;
 
-        int inputNum = input.nextInt();
-        if (inputNum < 5) {
-            System.out.println("Enjoy the good luck a friend brings you");
-        } else {
-            System.out.println("Your shoe selection will make you very happy today");
+        while(isOnRepeat) {
+            System.out.println("would you like to take this song off of repeat? if so, answer yes");
+            String userInput = input.next();
+
+            if (userInput.equals("yes")) {
+                isOnRepeat = false;
+            }
         }
+        System.out.println("play next song");
     }
 }
